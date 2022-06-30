f1a0b14982f57e0edbfbc5050099b1012b6f734e
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2022-06-30 08:58:37 -0300

    Remove .sql files from github

--- db_queroler.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS queroler_db;
-USE queroler_db;
-CREATE TABLE IF NOT EXISTS doadores (
-    nome VARCHAR(50) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    PRIMARY KEY(nome)
-);
-INSERT INTO doadores (nome, email) VALUES
-("Rodrigo Didier Anderson", "didier.rda@gmail.com");
